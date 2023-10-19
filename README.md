fbaa0d817a8a0f9b311f2df1de0c597c71d8c3bb
melektron <matteo@elektron.work>
2023-10-19 19:39:27 +0200

    finished implementation of digi4school for now and added proper README info

--- README.md
+++ README.md
@@ -1,7 +1,81 @@
 # Offline Academy
-Chrome extension to save Cisco NetAcad Lessons as Markdown
 
-## Links
+Browser extension which adds save/download buttons to some locked-ecosystem websites and eBook readers to allow saving their content offline as regular files.
+
+Currently supported browsers:
+
+- Chromium-based browsers
+
+(Support for others should be trivial to implement, feel free to add it in a PR)
+
+Currently supported websites:
+
+- Cisco Network Academy (NetAcad)
+- Digi4School eBook reader for Austrian school books
+
+
+## Downloading from Cisco NetAcad
+
+The extension adds a download button to the toolbar on every Cisco NetAcad course. The button will open a folder picker, as it needs to save multiple files. 
+
+After selecting the folder, it save the currently open course section as a simple HTML document which could even be represented using markdown. Any assets such as images and graphics encountered are also automatically saved in the neighboring assets folder.
+
+Embedded codeblocks (which are simply html "code" tags) are preserved as is.
+
+NetAcad has the annoyance of interactive elements such as tabs in their corses to e.g. compare different network protocols and show specifics for each. Offline Academy automatically clicks through any tablists and other simple interactive elements, saving the text and assets from each tab in a static format represented in the HTML document using subheadings.
+
+Complex interactive elements such as exams and interactive code blocks, as well as attached PacketTracer documents are not saved (although PacketTracer document download may be added, feel free to submit a PR ;) ).
+
+
+## Downloading from Digi4School
+
+The extension adds a download button to the toolbar on every digi4school ebook. This button will download the current page as an SVG file (or two pages when in dual-page mode). All content is embedded in the file -> no internet connection required to read it after the download.
+
+It may be useful to get the page as a PDF. Currently, this is not done automatically by the extension at this point (I couldn't get it to work), though I would like to add that feature in the future (Feel free to submit PR if you have any ideas).
+
+For now, you can use ```rsvg-convert``` to convert the SVG to a PDF:
+
+```bash
+# According to https://superuser.com/questions/381125/how-do-i-convert-an-svg-to-a-pdf-on-linux
+rsvg-convert -o book_pg50.pdf book_pg50.svg
+```
+
+The tool is included with ```librsvg2``` which you should be able to get with most package managers:
+
+```bash
+sudo apt install librsvg2-bin
+sudo pacman -S librsvg
+# ... More: https://command-not-found.com/rsvg-convert
+```
+
+(If you are using Windows simply switch to Linux).
+
+
+## Installing the extension
+
+This extension is not distributed on any stores. You need to build and install it yourself. The repository provides a webpack configuration which does all building automagically, so you just need to clone the repo, install dependencies and run the build target using npm:
+
+```bash
+git clone https://github.com/melektron/offline_academy.git
+cd offline_academy
+npm i
+npm run build
+```
+
+All build artifacts are placed in the ```dist``` folder. 
+
+(Currently, as only chromium-based browsers are supported, this folder is the unpacked chrome extension, although subfolders may be added for more browsers in the future.)
+
+### Install in chromium-based browsers
+
+You can load the unpacked extensions by selecting the ```dist``` folder according to [Google's developer documentation](https://developer.chrome.com/docs/extensions/mv3/getstarted/development-basics/#load-unpacked).
+
+
+## Contributions
+
+I am open to contributions of any kind, so if you have any feature ideas, bugfixes or typo fixes feel free to submit an Issue or - even better - a Pull Request.
+
+## Development notes and links
 
 Initial guidance on setting up typescript:
 https://betterprogramming.pub/creating-chrome-extensions-with-typescript-914873467b65
--- src/digi4school_content/digi4school_content.ts
+++ src/digi4school_content/digi4school_content.ts
@@ -11,54 +11,15 @@ Content script that adds download functionality to digi4school books.
 // import PDFDocument from "pdfkit"
 // import SVGtoPDF from "svg-to-pdfkit"
 // import blobStream from "blob-stream"
-import { sleep } from "utils/sleep"
 
-const ASSETS_DIR_NAME = "assets"
-const ASSET_BASE_PATH = "./" + ASSETS_DIR_NAME + "/";
-
-let main_directory_handle: FileSystemDirectoryHandle | undefined = undefined;
-
-/**
- * class representing an asset name and corresponding binary blob
- */
-class Asset {
-    constructor(
-        public name: string,
-        public data: Blob
-    ) {}
-}
-
-/**
- * asset that isn't necessarily done fetching and therefore contains a promise for the data 
- */
-class LoadingAsset {
-    constructor(
-        public name: string | undefined,
-        public data: Promise<Asset>
-    ) {}
-}
 
 class BookPage {
-
-    public output_document: HTMLDivElement;
-    public assets: LoadingAsset[];
-    public module_index: number = 0;
-    public module_title: string = "";
-    public section_index: number = 0;
-    public section_title: string = "";
-
-    constructor(
-    ) {
-        this.output_document = document.createElement("div");
-        this.assets = new Array<LoadingAsset>;
-    }
-
     /**
      * gets the current page's SVG document and processes it
      */
     async processCurrent() {
         let objects = document.querySelectorAll("#contentContainer object");
-        objects.forEach((object, index, parent) => {
+        for (const object of objects) {
             if (!(object instanceof HTMLObjectElement))
                 return;
             const svg_element_original = object.contentDocument?.querySelector("svg");
@@ -67,7 +28,7 @@ class BookPage {
 
             const svg_element = svg_element_original.cloneNode(true) as SVGSVGElement;
 
-            this.processImages(svg_element);
+            await this.processImages(svg_element);
 
             // TODO: convert to PDF https://pdfkit.org/docs/getting_started.html
 
@@ -85,132 +46,48 @@ class BookPage {
             document.body.appendChild(download_link);
             download_link.click();
             document.body.removeChild(download_link);
-        });
+        };
     }
 
     /**
      * Processes all images in an SVG document to replace external links with embeds
-     * https://stackoverflow.com/questions/934012/get-image-data-url-in-javascript
      * @param target_svg target SVG element to modify
      */
-    processImages(target_svg: SVGSVGElement) {
+    async processImages(target_svg: SVGSVGElement) {
         const images = target_svg.querySelectorAll("image");
-        images.forEach((element, key, parent) => {
-            console.log(element);
-        });
-    }
-
-    processImage(target_image: SVGImageElement) {
-    }
-
-}
-
-/**
- * downloads an asset from a provided url asynchronously
- * 
- * @param link link to the asset. This doesn't need to contain the base URI,
- * just the path relative the the website root is fine. (e.g. "/content/imageabc.jpg")
- * @returns an object containing the file name and a promise to the response containing the file contents
- */
-function downloadAsset(link: string): LoadingAsset {
-    const asset_uri = new URL(link, document.baseURI);
-    const asset_name = asset_uri.href.split("/").pop();
-    console.log("Fetching Asset: ", asset_uri.href);
-
-    // don't even try to fetch the asset if we can't determine the asset name
-    if (asset_name == undefined) {
-        return new LoadingAsset(
-            asset_name,
-            Promise.reject(new Error(`Invalid asset name, couldn't fetch: ${asset_uri.href}`))
-        );
-    }
-
-    return new LoadingAsset(
-        asset_name,
-        new Promise<Asset>((resolve, reject) => {
-            fetch(asset_uri)
-                .catch(async reason => reject(reason))
-                .then(async value => {
-                    if (value == null) {
-                        reject(new Error("Asset fetch operation delivered no result"));
-                        return;
-                    }
-                    if (!value.ok) {
-                        reject(new Error(`Asset fetch failed with code: ${value.status}`));
-                        return;
-                    }
-                    // fetch ok
-                    resolve(new Asset(
-                        asset_name,
-                        await value.blob()
-                    ));
-
-                });
-        })
-    );
-}
-
-
-/**
- * Saves the current book page as a PDF.
- */
-async function savePage(_page: BookPage) {
-    /*
-    // first, wait for all the content to finish downloading or failed
-    let settled_assets = await Promise.allSettled(_page.assets.map(a => a.data));
-    console.log("assets finished downloading: ", settled_assets);
-    
-    // main directory handle acquire used to be here
-    if (main_directory_handle == null)
-        throw new Error("Unknown save directory");
-
-    // @ts-ignore open the module directory
-    const module_directory_name = _section.module_index + "_" + _section.module_title.replaceAll(" ", "_");
-    const module_directory_handle = await main_directory_handle.getDirectoryHandle(module_directory_name, {
-        create: true
-    });
-
-    // @ts-ignore save the section file
-    const section_file_name = _section.module_index + "_" + _section.section_index + "_" + _section.section_title.replaceAll(" ", "_") + ".html";
-    const section_file_handle = await module_directory_handle.getFileHandle(section_file_name, {
-        create: true
-    });
-    // @ts-ignore for some reason  createWritable is also not found by TS
-    const section_file_writable  = await section_file_handle.createWritable();
-    await section_file_writable.write(
-        "<html><body>" +
-        _section.output_document.innerHTML + 
-        "</body></html>"
-    );
-    await section_file_writable.close();
-
-
-    // open the asset directory 
-    const asset_directory_handle = await module_directory_handle.getDirectoryHandle("assets", {
-        create: true
-    });
-    // save all the assets asynchronously
-    for (const asset of settled_assets) {
-        if (asset.status === "rejected") {
-            console.error("Failed to download an asset (unknown name)");
-            continue;
+        for (const element of images) {
+            // process all images
+            await this.processImage(element);
         }
-        console.log("Saving asset: ", asset.value.name);
-        const file_handle = await asset_directory_handle.getFileHandle(asset.value.name, {
-            create: true
-        });
-        // @ts-ignore for some reason createWritable is also not found by TS
-        const file_writable  = await file_handle.createWritable();
-        file_writable.write(asset.value.data).then(async () => {
-            console.log("Done saving asset: ", asset.value.name);
-            await file_writable.close();
-        }).catch(async () => {
-            console.error("Failed to save asset: ", asset.value.name);
+    }
+
+    /**
+     * Replaces an SVG image's externally linked source with an equivalent data URL
+     * @param target_image SVG image element to modify
+     * @returns 
+     */
+    async processImage(target_image: SVGImageElement) {
+        // https://stackoverflow.com/a/20285053
+        // convert image to a data url so image data can be embedded
+        return new Promise<void>((resolve, reject) => {
+            let xhr = new XMLHttpRequest();
+            xhr.onload = function() {
+                let reader = new FileReader();
+                reader.onloadend = function() {
+                    // change the image to use the generated data url
+                    target_image.setAttribute("xlink:href", reader.result as string);
+                    // async return
+                    resolve()
+                }
+                reader.readAsDataURL(xhr.response);
+            };
+            xhr.open('GET', target_image.href.baseVal);
+            xhr.responseType = 'blob';
+            xhr.send();
         });
     }
-*/
-}
 
+}
 
 // this function must not be async or else we get the error "Must be handling a user gesture to show a file picker."
 async function buttonCallback() {
